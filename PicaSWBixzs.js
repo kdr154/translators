a4e231bbbe1a49af4728c41689ef90af4f03b679
kdr154 <rosenkranz@uni-tuebingen.de>
2018-09-12 16:45:28 +0200

    reb

--- PicaSWBixzs.js
+++ PicaSWBixzs.js
@@ -900,6 +900,7 @@ var journalMapping = {
 	"1475-3065" : "!097935182!", // Scottish Journal of Theology
 	"0036-9306" : "!097935182!", // Scottish Journal of Theology
 	"1758-6623" : "!273867849!", // The Ecumenical Review
+	"0101-8434" : "!015182908!", // Revista eclesiástica brasileira
 
 	
 };
@@ -2032,6 +2033,7 @@ var issnPhysicalFormMapping = {
 	"0036-9306, 1475-3065" : "O", // Scottish Journal of Theology
 	"1475-3065" : "O", // Scottish Journal of Theology
 	"0036-9306" : "O", // Scottish Journal of Theology
+	"0101-8434" : "A", // Revista eclesiástica brasileira
 
 	
 	
